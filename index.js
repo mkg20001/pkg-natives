00b82d606bdc17169acb1b4f934979c436a78fc1
mkg20001 <mkg20001@gmail.com>
2017-09-08 15:10:17 +0200

    Split up the files

--- index.js
+++ index.js
@@ -5,56 +5,6 @@
 const pkg = require("pkg")
 const fs = require("fs")
 const path = require("path")
-const natives = ['assert',
-  'buffer',
-  'bindings',
-  'addons',
-  'child_process',
-  'cluster',
-  'cli',
-  'console',
-  'crypto',
-  'debugger',
-  'deprecations',
-  'dns',
-  'domain',
-  'errors',
-  'events',
-  'fs',
-  'globals',
-  'http',
-  'https',
-  'modules',
-  'net',
-  'os',
-  'path',
-  'process',
-  'punycode',
-  'querystring',
-  'readline',
-  'repl',
-  'stream',
-  'string_decoder',
-  'timers',
-  'tls',
-  'tracing',
-  'tty',
-  'dgram',
-  'url',
-  'util',
-  'v8',
-  'vm',
-  'zlib'
-].concat(["module"])
-
-function getMatches(string, regex, index) {
-  index = index || 1 // default to the first capturing group
-  var matches = []
-  var match
-  while ((match = regex.exec(string)))
-    matches.push(match[index])
-  return matches
-}
 
 const mod = process.argv[2] || process.cwd()
 const pjson = path.join(mod, "package.json")
@@ -62,123 +12,9 @@ const pdata = require(pjson)
 
 console.log("Pkg'ing module %s...", pdata.name)
 
-const reqex = /require\(["'](.+)["']\)/gi
-
-const log = require("debug")("pkg-natives")
-
-function getTree(file, paths, cache) {
-  if (!cache) cache = {}
-  let content
-  if (Buffer.isBuffer(file)) {
-    content = file.toString()
-    file = "__"
-  } else {
-    if (typeof file != "string") return {
-      file,
-      error: "Not a path"
-    }
-    if (cache[file]) {
-      log("cached dependency", file)
-      return cache[file]
-    }
-    log("reading %s", file)
-    content = fs.readFileSync(file)
-  }
-  const requires = getMatches(content, reqex).map(r => {
-    if (r.indexOf("'") == -1) return r
-    return r.split("'")[0]
-  })
-  const res = {
-    file,
-    requires: requires.map(name => {
-      if (natives.indexOf(name) != -1) return {
-        native: name
-      }
-      if (fs.existsSync(path.resolve(path.dirname(file), name)) && fs.lstatSync(path.resolve(path.dirname(file), name)).isDirectory() && fs.existsSync(path.resolve(path.dirname(file), name, "index.js"))) {
-        return {
-          file: fs.existsSync(path.resolve(path.dirname(file), name, "index.js"))
-        }
-      } else
-      if (fs.existsSync(path.resolve(path.dirname(file), name))) {
-        return {
-          file: path.resolve(path.dirname(file), name)
-        }
-      } else if (fs.existsSync(path.resolve(path.dirname(file), name + ".js"))) {
-        return {
-          file: path.resolve(path.dirname(file), name + ".js")
-        }
-      } else if (fs.existsSync(path.join(paths.node, name))) {
-        let p = path.join(paths.node, name)
-        if (fs.existsSync(path.resolve(path.dirname(file), name)) && fs.lstatSync(path.resolve(path.dirname(file), name)).isDirectory() && fs.existsSync(path.resolve(path.dirname(file), name, "index.js"))) {
-          return {
-            file: path.resolve(path.dirname(file), name, "index.js")
-          }
-        } else
-        if (fs.existsSync(p) && fs.lstatSync(p).isFile()) {
-          return {
-            file: p
-          }
-        } else if (fs.existsSync(p + ".js") && fs.lstatSync(p + ".js").isFile()) {
-          return {
-            file: p + ".js"
-          }
-        } else if (fs.lstatSync(p).isDirectory() && fs.existsSync(path.join(p, "package.json"))) {
-          p = path.resolve(p, require(path.join(p, "package.json")).main || "index.js")
-          if (fs.existsSync(p) && fs.lstatSync(p).isFile()) {
-            return {
-              file: p
-            }
-          } else if (fs.existsSync(p + ".js") && fs.lstatSync(p + ".js").isFile()) {
-            return {
-              file: p + ".js"
-            }
-          }
-        }
-      } else {
-        log("failed to resolve", name, "from", file)
-        return {
-          unres: file
-        }
-      }
-    })
-  }
-  cache[file] = res
-  res.requires = res.requires.map(f => {
-    if (!f) return {
-      error: "Empty"
-    }
-    if (f.unres) {
-      return {
-        unresolved: true,
-        file: f.unres
-      }
-    } else if (f.native) {
-      return {
-        native: true,
-        file: f.native
-      }
-    } else {
-      if (typeof f.file == "string" && fs.lstatSync(f.file).isDirectory()) return {
-        file: f.file,
-        error: "EISDIR"
-      }
-      return getTree(f.file, paths, cache)
-    }
-  })
-  return res
-}
-
-function getFlatTree(files, paths) {
-  let c = {}
-  files.forEach(file => getTree(file, paths, c))
-  delete c.__
-  let r = {}
-  for (var pth in c) {
-    c[pth].file = path.relative(paths.main, pth)
-    r[c[pth].file] = c[pth]
-  }
-  return r
-}
+const {
+  getFlatTree
+} = require("./lib/tree")
 
 console.log("Scanning for natives...")
 
@@ -192,10 +28,6 @@ for (var pth in tree) {
   const b = obj.requires.filter(o => o.file == "bindings")
   if (b.length) btree[pth] = obj
 }
-/*console.log(require('util').inspect(btree, {
-  depth: 3,
-  colors: true
-}))*/
 
 const nfiles = Object.keys(btree)
 
@@ -203,7 +35,6 @@ console.log("Found natives in %s", nfiles.join(", "))
 
 let nat = []
 
-//const nre = /require\(["']bindings["']\)\(["'](.+)["']\)/gi
 nfiles.forEach(file => nat.push({
   file,
   native: file.split("/")[1]
